cbac2884640ab41b9a567778a23151d3c672be3c
lockerios <locklockzhang@gmail.com>
2015-11-05 15:36:57 +0800

    add inputAccessoryView

--- TextImageDemo/EditViewController.swift
+++ TextImageDemo/EditViewController.swift
@@ -13,12 +13,22 @@ class EditViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var saveBBI: UIBarButtonItem!
     
+    var toolbar: UIToolbar!
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         saveBBI.enabled = false
+        
+        toolbar = UIToolbar(frame: CGRectMake(0,0,SCREEN_WIDTH,44))
+        toolbar.items = [
+            UIBarButtonItem.init(title: "图片", style: .Plain, target: self, action: Selector("p_addImage")),
+            UIBarButtonItem.init(barButtonSystemItem: .FlexibleSpace, target: self, action: nil),
+            UIBarButtonItem.init(title: "收起", style: .Plain, target: self, action: Selector("p_hideKB")),
+        ]
+        textView.inputAccessoryView = toolbar
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -42,6 +52,14 @@ class EditViewController: UIViewController, UITextViewDelegate {
     }
 
     //MARK: - Methods
+    
+    func p_hideKB() {
+        textView.resignFirstResponder()
+    }
+    
+    func p_addImage() {
+        
+    }
     
     @IBAction func p_save(sender: AnyObject) {
         ItemModel.sharedItemModel.p_addItem(ItemEntity.init(itemString: textView.text))
